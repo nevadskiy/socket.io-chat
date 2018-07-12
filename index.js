02b4b7e5c09dbbad962d93fc0ae7fa2284b5ecb5
Nevadskiy <nevadskiy@gmail.com>
2018-07-12 21:54:28 +0300

    Require clients socket and vue

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+/** express */
 var app = require('express')();
 var server = require('http').Server(app);
+
+/** socket.io */
+var io = require('socket.io')(server);
 
 server.listen(3000);
 
 app.get('/', function(request, response) {
-  response.send('Hello world');
+  response.sendFile(__dirname + '/index.html');
 });
+
+io.on('connection', function(socket) {
+  console.log('A connection was made.');
+})
